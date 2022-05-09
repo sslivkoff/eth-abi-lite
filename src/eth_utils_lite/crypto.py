4363b6a3fa4351bff95b0d830e78451bd50f76b0
sslivkoff <stormslivkoff@gmail.com>
2022-05-09 13:15:07 -0700

    import keccak lazily

--- src/eth_utils_lite/crypto.py
+++ src/eth_utils_lite/crypto.py
@@ -1,6 +1,5 @@
 from typing import Union
 
-from Crypto.Hash import keccak as f_keccak
 
 from .conversions import to_bytes
 
@@ -8,5 +7,7 @@ from .conversions import to_bytes
 def keccak(
     primitive: Union[bytes, int, bool] = None, hexstr: str = None, text: str = None
 ) -> bytes:
+    from Crypto.Hash import keccak as f_keccak
+
     as_bytes = to_bytes(primitive, hexstr, text)
     return f_keccak.new(digest_bits=256, data=as_bytes).digest()
